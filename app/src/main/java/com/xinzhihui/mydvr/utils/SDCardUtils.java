21e1af914b66d652f4d4ad364535b2b15c3cb13a
qiansheng <qs65535@163.com>
2016-11-17 13:47:57 +0800

    4..4环境下只显示mounted的存储设备

--- app/src/main/java/com/xinzhihui/mydvr/utils/SDCardUtils.java
+++ app/src/main/java/com/xinzhihui/mydvr/utils/SDCardUtils.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 /**
  * SD卡相关的辅助类
@@ -135,23 +136,31 @@ public class SDCardUtils {
             Method getVolumeList = mStorageManager.getClass().getMethod("getVolumeList");
             Method getPath = storageVolumeClazz.getMethod("getPath");
             Method isRemovable = storageVolumeClazz.getMethod("isRemovable");
+            Method getState = storageVolumeClazz.getMethod("getState");
             Object result = getVolumeList.invoke(mStorageManager);
             final int length = Array.getLength(result);
 
-            String paths[] = new String[length];
+            ArrayList<String> pathList = new ArrayList<String>();
 
             for (int i = 0; i < length; i++) {
                 Object storageVolumeElement = Array.get(result, i);
                 String path = (String) getPath.invoke(storageVolumeElement);
 
-                paths[i] = path;
-
+                String state = (String) getState.invoke(storageVolumeElement);
+                if ("mounted".equals(state)) {
+                    pathList.add(path);
+                }
 //                boolean removable = (Boolean) isRemovable.invoke(storageVolumeElement);
 //                if (is_removale == removable) {
 //                    return path;
 //                }
             }
-            return paths;
+            if (pathList != null && pathList.size() > 0) {
+                String paths[] = new String[pathList.size()];
+                pathList.toArray(paths);
+                return paths;
+            }
+            return null;
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
